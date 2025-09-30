97a60f3bd1ccfb6da98a11aa617e1f7d8e76103f
Rezha <rezhasdf@gmail.com>
2025-09-30 19:25:56 +0700

    fix: EmployeeCollection method signature untuk Laravel 9
    Remove type hints dari toArray() & with() methods.
    Laravel 9 ResourceCollection parent class tidak pakai type hints.
    
    Fatal error resolved.

--- app/Http/Resources/EmployeeCollection.php
+++ app/Http/Resources/EmployeeCollection.php
@@ -14,8 +14,11 @@ class EmployeeCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
      */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
         return [
             'data' => EmployeeResource::collection($this->collection),
@@ -54,8 +57,11 @@ class EmployeeCollection extends ResourceCollection
 
     /**
      * Get additional data that should be returned with the resource array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
      */
-    public function with(Request $request): array
+    public function with($request)
     {
         return [
             'metadata' => [

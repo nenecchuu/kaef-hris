d3d3fec82dedf244d70007e81289da43f6e460cd
Rezha <rezhasdf@gmail.com>
2025-09-30 19:26:36 +0700

    fix: EmployeeResource method signature untuk Laravel 9
    Remove type hints dari toArray() method.
    Laravel 9 JsonResource parent class tidak pakai type hints.

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -14,8 +14,11 @@ class EmployeeResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
      */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
         return [
             'employee_id' => $this->employee_id,
